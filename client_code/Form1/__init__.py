153a72a2828617717e1d9f1ddba5e82cd14c2feb
scharr@sab-team.com <scharr@sab-team.com>
2024-02-02 10:04:35 +0000

    Cloned app

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    self.init_components(**properties)
+    self.repeating_panel_1.items=app_tables.images.search()
+
+  def file_loader_1_change(self, file, **event_args):
+    for fl in self.file_loader_1.files:
+      app_tables.images.add_row(image=fl)
+    open_form('Form1')
+
