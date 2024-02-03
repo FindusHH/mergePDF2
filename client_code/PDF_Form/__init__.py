d6f12b88a5ccd9c4e92bafb719f2c598ed9f7919
scharr@sab-team.com <scharr@sab-team.com>
2024-02-03 13:18:21 +0000

    Edited PDF_Form

--- client_code/PDF_Form/__init__.py
+++ client_code/PDF_Form/__init__.py
@@ -8,8 +8,9 @@ from anvil.tables import app_tables
 class PDF_Form(PDF_FormTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
-    self.repeating_panel_1.items=app_tables.files.search()
-
+    #self.repeating_panel_1.items=app_tables.files.search()
+    self.repeating_panel_1.items=app_tables.files.search(file_name = q.not_(None)) # __merged__.pdf
+    #q.ilike('%ion')  __merged__.pdf
 
   def file_loader_1_change(self, file, **event_args):
     for fl in self.file_loader_1.files:
