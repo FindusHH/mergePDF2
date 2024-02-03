96cc9d9e7a0a8103b983da22a3b47f08c4b7a70b
scharr@sab-team.com <scharr@sab-team.com>
2024-02-03 14:51:34 +0000

    TO DO prepare for sequence number

--- client_code/PDF_Form/__init__.py
+++ client_code/PDF_Form/__init__.py
@@ -11,10 +11,16 @@ class PDF_Form(PDF_FormTemplate):
     #self.repeating_panel_1.items=app_tables.files.search()
     self.repeating_panel_1.items=app_tables.files.search(file_name = q.not_(None)) # __merged__.pdf
 
+    sorted_items = sorted(self.repeating_panel_1.items, key=lambda k: k['sequence'], reverse=False)
+    self.repeating_panel_1.items = sorted_items
+
 
   def file_loader_1_change(self, file, **event_args):
     for fl in self.file_loader_1.files:
       file_name = fl.name
+      # TO DO
+      # retrieve highest sequence number
+      # assigne highest sequence number to new uploaded file
       app_tables.files.add_row(file=fl, file_name=file_name)
     open_form('PDF_Form')
 
