8830d47a802d8b487ba2318aa48b3a70c88170cd
scharr@sab-team.com <scharr@sab-team.com>
2024-02-04 19:59:20 +0000

    Edited PDF_Form

--- client_code/PDF_Form/__init__.py
+++ client_code/PDF_Form/__init__.py
@@ -16,12 +16,25 @@ class PDF_Form(PDF_FormTemplate):
 
 
   def file_loader_1_change(self, file, **event_args):
+    #sequence = 1
+    list_of_sequence_values =[]
+    # select max sequence value where sequence value < 99
+    for row in app_tables.files.search(file_name = q.not_(None)):
+      sequence = row["sequence"]
+      list_of_sequence_values.append(sequence)
+      print("sequence (UI) = " + str(sequence))
+    maximum = max(list_of_sequence_values)
+    print("Max Seq Val = " + str(maximum))
+    sequence = maximum + 1
+    
+    self.file_loader_1.file_types = "application/pdf"
     for fl in self.file_loader_1.files:
       file_name = fl.name
       # TO DO
       # retrieve highest sequence number
       # assigne highest sequence number to new uploaded file
-      app_tables.files.add_row(file=fl, file_name=file_name)
+      app_tables.files.add_row(file=fl, file_name=file_name, sequence=sequence)
+      sequence = sequence + 1
     open_form('PDF_Form')
 
   def button_1_click(self, **event_args):
