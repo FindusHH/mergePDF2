80d6e2a6c3197c4866a30a1b0233c7fc63b94d9e
scharr@sab-team.com <scharr@sab-team.com>
2024-02-05 10:28:11 +0000

    Edited PDF_Form.ItemTemplate1

--- client_code/PDF_Form/ItemTemplate1/__init__.py
+++ client_code/PDF_Form/ItemTemplate1/__init__.py
@@ -8,10 +8,21 @@ from anvil.tables import app_tables
 class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     self.init_components(**properties)
+    # show filename
     pic_name = self.item['file'].name
-    self.label_1.text = pic_name
+    self.label_filename.text = pic_name
+    
+    # show sequence number
+    seqno = self.item['sequence']
+    self.label_seqno.text = seqno
 
-  def button_1_click(self, **event_args):
+  def button_delete_click(self, **event_args):
     self.item.delete()
     open_form('PDF_Form')
+
+  def button_up_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    seqno = self.label_seqno.text
+    alert(seqno)
+    
 
